c648ebbaf50e18110d7ae6e1254b2a4d7d6d959a
Discord.js Arabian Developer <47581711+vzxcl@users.noreply.github.com>
2019-03-19 16:34:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,32 +6,31 @@ client.on('ready', () => {
 });
 
 
-const prefix = "*"
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '509243410234867724') return;
+  if (message.author.id !== '398919620993482753') return;
 
-if (message.content.startsWith(prefix + 'g')) {
+if (message.content.startsWith('g')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
 
-if (message.content.startsWith(prefix + 'w')) {
+if (message.content.startsWith('w')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
-if (message.content.startsWith(prefix + 'l')) {
+if (message.content.startsWith('l')) {
 client.user.setActivity(argresult, {type:'LISTENING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
 
-if (message.content.startsWith(prefix + 's')) {
+if (message.content.startsWith('s')) {
   client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 }
 
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.TOKEN);
